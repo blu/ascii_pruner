fc200fddde368ccb59b08a47b2eb251f6d85c09d
Martin Krastev <blu.dark@gmail.com>
2017-08-03 20:50:42 +0300

    clean up, part deux

--- prune.cpp
+++ prune.cpp
@@ -12,9 +12,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint8_t input[32] __attribute__ ((aligned(16))) = " 1  2  3    45  ";
+uint8_t input[32] __attribute__ ((aligned(16))) = "012345 789abcdef 123456789abcde";
 uint8_t output[32] __attribute__ ((aligned(16)));
-static const size_t misalign = 1; // simulate mis-alignment at write
 
 // print utility
 #if __aarch64__
@@ -94,44 +93,40 @@ inline void testee00() {
 }
 
 #if __aarch64__
-// sole pruner - ASIMD2 version; filter single blank per every N input chars, N = vector size
-inline void testee01() {
+// naive pruner, 16-batch; filter single blank from N input chars, followed by K optional trailing blanks, N + K = batch size
+// example: "1234 678  " -> "1234678" (N + K = 10)
+inline size_t testee01() {
 	uint8x16_t const vinput = vld1q_u8(input);
 	uint8x16_t prfsum = vcleq_u8(vinput, vdupq_n_u8(' '));
 
-	// before computing the actual prefix sum: count_of_blanks := vaddvq_u8(prfsum)
+	// pick one:
+	// before computing the prefix sum: count_of_blanks := vaddvq_u8(prfsum)
+	// after computing the prefix sum: count_of_blanks := prfsum[last_lane]
 	prfsum = vaddq_u8(prfsum, vextq_u8(vdupq_n_u8(0), prfsum, 16 - 1));
 	prfsum = vaddq_u8(prfsum, vextq_u8(vdupq_n_u8(0), prfsum, 16 - 2));
 	prfsum = vaddq_u8(prfsum, vextq_u8(vdupq_n_u8(0), prfsum, 16 - 4));
 	prfsum = vaddq_u8(prfsum, vextq_u8(vdupq_n_u8(0), prfsum, 16 - 8));
 
+	int8_t const bnum = vgetq_lane_u8(prfsum, 15);
+
 	uint8x16_t const index = vsubq_u8((uint8x16_t) { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, prfsum);
-
-#if QUIRK_001_MISSCHEDULED_TBL
-	// clang-3.7 produces worse scheduling when it encounters tbl -- put that in a black box
-	uint8x16_t res;
-	asm volatile ("tbl %0.16b, {%1.16b}, %2.16b"
-		: "=w" (res) : "w" (vinput), "w" (index) : );
-
-#else
 	uint8x16_t const res = vqtbl1q_u8(vinput, index);
 
-#endif
-	vst1q_u8(output + misalign, res);
-	// omitted from this test: output ptr needs to be advanced by count of non-blanks
+	vst1q_u8(output, res);
+	return sizeof(uint8x16_t) + bnum;
 }
 
-// sole pruner - ASIMD2 version, 32-batch; filter single blank per every N input chars, N = vector size
-inline void testee02() {
+// naive pruner, 32-batch; filter single blank from N input chars, followed by K optional trailing blanks, N + K = half batch size
+// example: "1234 678  " -> "1234678" (N + K = 10)
+inline size_t testee02() {
 	uint8x16_t const vinput0 = vld1q_u8(input);
 	uint8x16_t const vinput1 = vld1q_u8(input + sizeof(uint8x16_t));
 	uint8x16_t prfsum0 = vcleq_u8(vinput0, vdupq_n_u8(' '));
 	uint8x16_t prfsum1 = vcleq_u8(vinput1, vdupq_n_u8(' '));
 
-	// before computing the actual prefix sum: count_of_blanks := vaddvq_u8(prfsum)
-	int8_t const blen0 = vaddvq_u8(prfsum0);
-	int8_t const blen1 = vaddvq_u8(prfsum1);
-
+	// pick one:
+	// before computing the prefix sum: count_of_blanks := vaddvq_u8(prfsum)
+	// after computing the prefix sum: count_of_blanks := prfsum[last_lane]
 	prfsum0 = vaddq_u8(prfsum0, vextq_u8(vdupq_n_u8(0), prfsum0, 16 - 1));
 	prfsum1 = vaddq_u8(prfsum1, vextq_u8(vdupq_n_u8(0), prfsum1, 16 - 1));
 	prfsum0 = vaddq_u8(prfsum0, vextq_u8(vdupq_n_u8(0), prfsum0, 16 - 2));
@@ -141,59 +136,54 @@ inline void testee02() {
 	prfsum0 = vaddq_u8(prfsum0, vextq_u8(vdupq_n_u8(0), prfsum0, 16 - 8));
 	prfsum1 = vaddq_u8(prfsum1, vextq_u8(vdupq_n_u8(0), prfsum1, 16 - 8));
 
+	int8_t const bnum0 = vgetq_lane_u8(prfsum0, 15);
+	int8_t const bnum1 = vgetq_lane_u8(prfsum1, 15);
+
 	uint8x16_t const index0 = vsubq_u8((uint8x16_t) { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, prfsum0);
 	uint8x16_t const index1 = vsubq_u8((uint8x16_t) { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, prfsum1);
-
-#if QUIRK_001_MISSCHEDULED_TBL
-	// clang-3.7 produces worse scheduling when it encounters tbl -- put that in a black box
-	uint8x16_t res0, res1;
-	asm volatile (
-		"tbl %0.16b, {%2.16b}, %3.16b\n\t"
-		"tbl %1.16b, {%4.16b}, %5.16b"
-		: "=w" (res0), "=w" (res1)
-		: "w" (vinput0), "w" (index0),
-		  "w" (vinput1), "w" (index1) : );
-
-#else
 	uint8x16_t const res0 = vqtbl1q_u8(vinput0, index0);
 	uint8x16_t const res1 = vqtbl1q_u8(vinput1, index1);
 
-#endif
-	vst1q_u8(output + misalign, res0);
-	vst1q_u8(output + misalign + sizeof(uint8x16_t) + blen0, res1);
-	// omitted from this test: output ptr needs to be advanced by count of non-blanks
+	vst1q_u8(output, res0);
+	vst1q_u8(output + sizeof(uint8x16_t) + bnum0, res1);
+	return sizeof(uint8x16_t) * 2 + bnum0 + bnum1;
 }
 
 #elif __SSSE3__
-// sole pruner - SSSE3 version; filter single blank per every N input chars, N = vector size
-inline void testee01() {
+// naive pruner, 16-batch; filter single blank from N input chars, followed by K optional trailing blanks, N + K = batch size
+// example: "1234 678  " -> "1234678" (N + K = 10)
+inline size_t testee01() {
 	__m128i const vinput = _mm_load_si128(reinterpret_cast< const __m128i* >(input));
 	__m128i prfsum = _mm_cmplt_epi8(vinput, _mm_set1_epi8(' ' + 1));
 
-	// before computing the actual prefix sum: count_of_blanks := _mm_popcnt_u32(_mm_movemask_epi8(prfsum))
+	// pick one:
+	// before computing the prefix sum: count_of_blanks := _mm_popcnt_u32(_mm_movemask_epi8(prfsum))
+	// after computing the prefix sum: count_of_blanks := prfsum[last_lane]
 	prfsum = _mm_add_epi8(prfsum, _mm_slli_si128(prfsum, 1));
 	prfsum = _mm_add_epi8(prfsum, _mm_slli_si128(prfsum, 2));
 	prfsum = _mm_add_epi8(prfsum, _mm_slli_si128(prfsum, 4));
 	prfsum = _mm_add_epi8(prfsum, _mm_slli_si128(prfsum, 8));
 
+	int8_t const bnum = uint16_t(_mm_extract_epi16(prfsum, 7)) >> 8;
+
 	__m128i const index = _mm_sub_epi8(_mm_setr_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15), prfsum);
 	__m128i const res = _mm_shuffle_epi8(vinput, index);
 
-	_mm_storeu_si128(reinterpret_cast< __m128i* >(output + misalign), res);
-	// omitted from this test: output ptr needs to be advanced by count of non-blanks
+	_mm_storeu_si128(reinterpret_cast< __m128i* >(output), res);
+	return sizeof(__m128i) + bnum;
 }
 
-// sole pruner - SSSE3 version, 32-batch; filter single blank per every N input chars, N = vector size
-inline void testee02() {
+// naive pruner, 32-batch; filter single blank from N input chars, followed by K optional trailing blanks, N + K = half batch size
+// example: "1234 678  " -> "1234678" (N + K = 10)
+inline size_t testee02() {
 	__m128i const vinput0 = _mm_load_si128(reinterpret_cast< const __m128i* >(input));
 	__m128i const vinput1 = _mm_load_si128(reinterpret_cast< const __m128i* >(input) + 1);
 	__m128i prfsum0 = _mm_cmplt_epi8(vinput0, _mm_set1_epi8(' ' + 1));
 	__m128i prfsum1 = _mm_cmplt_epi8(vinput1, _mm_set1_epi8(' ' + 1));
 
-	// before computing the actual prefix sum: count_of_blanks := _mm_popcnt_u32(_mm_movemask_epi8(prfsum))
-	uint32_t const blen0 = _mm_popcnt_u32(_mm_movemask_epi8(prfsum0));
-	uint32_t const blen1 = _mm_popcnt_u32(_mm_movemask_epi8(prfsum1));
-
+	// pick one:
+	// before computing the prefix sum: count_of_blanks := _mm_popcnt_u32(_mm_movemask_epi8(prfsum))
+	// after computing the prefix sum: count_of_blanks := prfsum[last_lane]
 	prfsum0 = _mm_add_epi8(prfsum0, _mm_slli_si128(prfsum0, 1));
 	prfsum1 = _mm_add_epi8(prfsum1, _mm_slli_si128(prfsum1, 1));
 	prfsum0 = _mm_add_epi8(prfsum0, _mm_slli_si128(prfsum0, 2));
@@ -203,20 +193,24 @@ inline void testee02() {
 	prfsum0 = _mm_add_epi8(prfsum0, _mm_slli_si128(prfsum0, 8));
 	prfsum1 = _mm_add_epi8(prfsum1, _mm_slli_si128(prfsum1, 8));
 
+	int8_t const bnum0 = uint16_t(_mm_extract_epi16(prfsum0, 7)) >> 8;
+	int8_t const bnum1 = uint16_t(_mm_extract_epi16(prfsum1, 7)) >> 8;
+
 	__m128i const index0 = _mm_sub_epi8(_mm_setr_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15), prfsum0);
 	__m128i const index1 = _mm_sub_epi8(_mm_setr_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15), prfsum1);
 	__m128i const res0 = _mm_shuffle_epi8(vinput0, index0);
 	__m128i const res1 = _mm_shuffle_epi8(vinput1, index1);
 
-	_mm_storeu_si128(reinterpret_cast< __m128i* >(output + misalign), res0);
-	_mm_storeu_si128(reinterpret_cast< __m128i* >(output + misalign + sizeof(__m128i) - blen0), res1);
-	// omitted from this test: output ptr needs to be advanced by count of non-blanks
+	_mm_storeu_si128(reinterpret_cast< __m128i* >(output), res0);
+	_mm_storeu_si128(reinterpret_cast< __m128i* >(output + sizeof(__m128i) + bnum0), res1);
+	return sizeof(__m128i) * 2 + bnum0 + bnum1;
 }
 
 #endif
-#if __SSE2__
-// pruner semi -- replace blanks with the next non-blank
-inline void testee03() {
+#if __SSE2__ && __POPCNT__
+// pruner semi, 16-batch; replace blanks with the next non-blank, cutting off trailing blanks from the batch
+// example: "1234 678  " -> "12346678"
+inline size_t testee03() {
 	__m128i const vin = _mm_load_si128(reinterpret_cast< const __m128i* >(input));
 
 	// discover non-blanks
@@ -243,17 +237,19 @@ inline void testee03() {
 	// add blank counts to a sequential index to get non-blanks index
 	__m128i const index = _mm_add_epi8(_mm_setr_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15), prfmax);
 
-	// cap trailing out-of-bounds in the index (non-mandatory)
+	// cap trailing out-of-bounds in the index
 	__m128i const indey = _mm_or_si128(index, _mm_cmpgt_epi8(index, _mm_set1_epi8(15)));
 
 	// use the index to fetch all non-blanks from the dictionary
 	__m128i const res = _mm_shuffle_epi8(vin, indey);
 
 	_mm_storeu_si128(reinterpret_cast< __m128i* >(output), res);
+	return sizeof(__m128i) - _mm_popcnt_u32(_mm_movemask_epi8(indey));
 }
 
 #endif
-#if __SSSE3__
+#if __aarch64__
+#elif __SSSE3__
 // pruner -- full-fledged; SSSE3 version
 inline void testee04() {
 }
@@ -284,13 +280,7 @@ int main(int, char**) {
 		asm volatile ("" : : : "memory");
 	}
 
-#if TESTEE
-	fprintf(stderr, "%.32s\n", output + misalign);
-
-#else
 	fprintf(stderr, "%.32s\n", output);
-
-#endif
 	return 0;
 }
 
