74116819b329095f41eb54b36c968c0851089c5a
Martin Krastev <blu.dark@gmail.com>
2017-08-11 01:32:03 +0300

    add permute latency test lattest

---
+++ lattest.cpp
@@ -0,0 +1,103 @@
+// latency accumulator -- a chain of ops with data dependency between each two ops;
+// to break the dependency set COISSUE
+
+#if __aarch64__ == 0 && __SSSE3__ == 0
+	#error wrong target arch
+#endif
+
+#include <stddef.h>
+
+int main(int, char**) {
+	size_t const rep = size_t(5e8);
+
+	for (size_t i = 0; i < rep; ++i) {
+
+#if __aarch64__
+#if COISSUE
+		asm volatile (
+			"tbl v17.16b, {v1.16b},  v0.16b\n\t"
+			"tbl v18.16b, {v2.16b},  v0.16b\n\t"
+			"tbl v19.16b, {v3.16b},  v0.16b\n\t"
+			"tbl v20.16b, {v4.16b},  v0.16b\n\t"
+			"tbl v21.16b, {v5.16b},  v0.16b\n\t"
+			"tbl v22.16b, {v6.16b},  v0.16b\n\t"
+			"tbl v23.16b, {v7.16b},  v0.16b\n\t"
+			"tbl v24.16b, {v8.16b},  v0.16b\n\t"
+			"tbl v25.16b, {v9.16b},  v0.16b\n\t"
+			"tbl v26.16b, {v10.16b}, v0.16b\n\t"
+			"tbl v27.16b, {v11.16b}, v0.16b\n\t"
+			"tbl v28.16b, {v12.16b}, v0.16b\n\t"
+			"tbl v29.16b, {v13.16b}, v0.16b\n\t"
+			"tbl v30.16b, {v14.16b}, v0.16b\n\t"
+			"tbl v31.16b, {v15.16b}, v0.16b\n\t"
+			"tbl v0.16b,  {v16.16b}, v0.16b"
+			: : : "memory");
+
+#else
+		asm volatile (
+			"tbl v2.16b,  {v1.16b},  v0.16b\n\t"
+			"tbl v3.16b,  {v2.16b},  v0.16b\n\t"
+			"tbl v4.16b,  {v3.16b},  v0.16b\n\t"
+			"tbl v5.16b,  {v4.16b},  v0.16b\n\t"
+			"tbl v6.16b,  {v5.16b},  v0.16b\n\t"
+			"tbl v7.16b,  {v6.16b},  v0.16b\n\t"
+			"tbl v8.16b,  {v7.16b},  v0.16b\n\t"
+			"tbl v9.16b,  {v8.16b},  v0.16b\n\t"
+			"tbl v10.16b, {v9.16b},  v0.16b\n\t"
+			"tbl v11.16b, {v10.16b}, v0.16b\n\t"
+			"tbl v12.16b, {v11.16b}, v0.16b\n\t"
+			"tbl v13.16b, {v12.16b}, v0.16b\n\t"
+			"tbl v14.16b, {v13.16b}, v0.16b\n\t"
+			"tbl v15.16b, {v14.16b}, v0.16b\n\t"
+			"tbl v16.16b, {v15.16b}, v0.16b\n\t"
+			"tbl v17.16b, {v16.16b}, v0.16b"
+			: : : "memory");
+
+#endif
+#elif __SSSE3__
+#if COISSUE
+		asm volatile (
+			"pshufb %%xmm1,  %%xmm0\n\t"
+			"pshufb %%xmm2,  %%xmm0\n\t"
+			"pshufb %%xmm3,  %%xmm0\n\t"
+			"pshufb %%xmm4,  %%xmm0\n\t"
+			"pshufb %%xmm5,  %%xmm0\n\t"
+			"pshufb %%xmm6,  %%xmm0\n\t"
+			"pshufb %%xmm7,  %%xmm0\n\t"
+			"pshufb %%xmm8,  %%xmm0\n\t"
+			"pshufb %%xmm9,  %%xmm0\n\t"
+			"pshufb %%xmm10, %%xmm0\n\t"
+			"pshufb %%xmm11, %%xmm0\n\t"
+			"pshufb %%xmm12, %%xmm0\n\t"
+			"pshufb %%xmm13, %%xmm0\n\t"
+			"pshufb %%xmm14, %%xmm0\n\t"
+			"pshufb %%xmm15, %%xmm0\n\t"
+			"pshufb %%xmm0,  %%xmm0"
+			: : : "memory");
+
+#else
+		asm volatile (
+			"pshufb %%xmm1,  %%xmm0\n\t"
+			"pshufb %%xmm2,  %%xmm0\n\t"
+			"pshufb %%xmm3,  %%xmm0\n\t"
+			"pshufb %%xmm4,  %%xmm0\n\t"
+			"pshufb %%xmm5,  %%xmm0\n\t"
+			"pshufb %%xmm6,  %%xmm0\n\t"
+			"pshufb %%xmm7,  %%xmm0\n\t"
+			"pshufb %%xmm8,  %%xmm0\n\t"
+			"pshufb %%xmm9,  %%xmm0\n\t"
+			"pshufb %%xmm10, %%xmm0\n\t"
+			"pshufb %%xmm11, %%xmm0\n\t"
+			"pshufb %%xmm12, %%xmm0\n\t"
+			"pshufb %%xmm13, %%xmm0\n\t"
+			"pshufb %%xmm14, %%xmm0\n\t"
+			"pshufb %%xmm15, %%xmm0\n\t"
+			"pshufb %%xmm0,  %%xmm0"
+			: : : "memory");
+
+#endif
+#endif
+    }
+
+    return 0;
+}
---
+++ lattest.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ -z `which time` ]; then
+	echo "error: time not found"
+	exit 255
+fi
+if [ -z `which bc` ]; then
+	echo "error: bc not found"
+	exit 254
+fi
+
+CFLAGS=(
+	-Ofast
+	-fno-rtti
+	-fno-exceptions
+	-fstrict-aliasing
+)
+
+if [[ $HOSTTYPE == "arm" ]]; then
+	if [ -z $CC ] || [ ! -e $CC ]; then
+		echo "error: envvar CC must hold the path to aarch64 compiler"
+		exit 252
+	fi
+elif [[ $HOSTTYPE == "aarch64" ]]; then
+	if [ -z $CC ]; then
+		CC=g++
+	fi
+	if [ -z `which $CC` ]; then
+		echo "error: $CC not found"
+		exit 253
+	fi
+elif [[ $HOSTTYPE == "x86_64" ]]; then
+	if [ -z $CC ]; then
+		CC=g++
+	fi
+	if [ -z `which $CC` ]; then
+		echo "error: $CC not found"
+		exit 253
+	fi
+	CFLAGS+=(
+		-mssse3
+	)
+else
+	echo "error: unsupported host type"
+	exit 251
+fi
+
+${CC} ${CFLAGS[@]} lattest.cpp -o lattest_nocoissue
+`which time` -f %e ./lattest_nocoissue 2>&1 | xargs -i echo "scale=4;" {} " * " `lscpu | grep -m 1 -E "^CPU[[:space:][:alpha:]]+MHz" | sed "s/^[^:]\+://g"` " * 10^6 / (5 * 10^8 * 16)" | bc
+
+${CC} ${CFLAGS[@]} lattest.cpp -o lattest_coissue -DCOISSUE
+`which time` -f %e ./lattest_coissue 2>&1 | xargs -i echo "scale=4;" {} " * " `lscpu | grep -m 1 -E "^CPU[[:space:][:alpha:]]+MHz" | sed "s/^[^:]\+://g"` " * 10^6 / (5 * 10^8 * 16)" | bc
